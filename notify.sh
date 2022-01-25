856f4a2196e4a69d7e51ec56115d4f5e3312d26a
Tim <tuktamyshev.t0308@gmail.com>
2022-01-25 18:50:35 +0500

    added commit message

--- notify.sh
+++ notify.sh
@@ -8,11 +8,11 @@ fi
 
 TIME="10"
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
-TEXT="Starting pipeline: 🚀%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG%0ATag:+$CI_COMMIT_TAG%0ACommit:$CI_COMMIT_SHORT_SHA"
+TEXT="Starting pipeline: 🚀%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG%0ATag:+$CI_COMMIT_TAG%0ACommit:+$CI_COMMIT_SHORT_SHA%0ACommit Message:+$CI_COMMIT_MESSAGE"
 
 
 if [[ "$1" == "end" ]]; then
-    TEXT="Pipeline status: $3%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG%0ATag:+$CI_COMMIT_TAG%0ACommit:$CI_COMMIT_SHORT_SHA"
+    TEXT="Pipeline status: $3%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG%0ATag:+$CI_COMMIT_TAG%0ACommit:+$CI_COMMIT_SHORT_SHA%0ACommit Message:+$CI_COMMIT_MESSAGE"
 fi
 
 curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT&disable_notification=$SILENT" $URL > /dev/null
