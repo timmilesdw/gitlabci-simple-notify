24eff643c2197cf2af328f75d339eb7a0d5c7f41
Tim <tuktamyshev.t0308@gmail.com>
2022-01-25 16:55:25 +0500

    added silent and README.md

--- README.md
+++ README.md
@@ -3,4 +3,55 @@
 Simple notifications from Gitlab CI to telegram
 
 # Usage
-Work in progress
+```bash
+docker run timmiles/gitlabci-simple-notify <start|end> <silent | "">
+```
+
+```yaml
+stages:
+  - Notify Start
+  - Generate Changelog
+  - Build
+  - Notify End
+
+.notify_template: &notify_configuration
+    image: timmiles/gitlabci-simple-notify
+
+Notify Start:
+    stage: Notify Start
+    <<: *notify_configuration
+    script:
+      - start
+
+Generate Changelog:
+  stage: Generate Changelog
+  image:
+    name: quay.io/git-chglog/git-chglog:0.15.1
+    entrypoint: [""]
+  script:
+    # Generate changelog logic
+
+Build:
+  only:
+    - tags
+  stage: Build
+  image:
+    name: gcr.io/kaniko-project/executor:debug
+    entrypoint: [""]
+  script:
+    # build logic
+
+Notify Success:
+    stage: Notify End
+    <<: *notify_configuration
+    script:
+      - end ✅
+
+Notify Fail:
+    stage: Notify End
+    <<: *notify_configuration
+    script:
+      - end ❌
+    when: on_failure
+
+```
--- notify.sh
+++ notify.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env sh
 
-if [[ "$1" == "end" ]]; then
-    TIME="10"
-    URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
-    TEXT="Pipeline status: $2%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG%0ATag:+$CI_COMMIT_TAG%0ACommit:$CI_COMMIT_SHORT_SHA"
+SILENT = 0
 
-    curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
-else
-    TIME="10"
-    URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
-    TEXT="Starting pipeline: %0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG%0ATag:+$CI_COMMIT_TAG%0ACommit:$CI_COMMIT_SHORT_SHA"
-
-    curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
+if [["$2" == "silent"]]; then
+   SILENT = 1
 fi
 
+TIME="10"
+URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
+TEXT="Starting pipeline: %0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG%0ATag:+$CI_COMMIT_TAG%0ACommit:$CI_COMMIT_SHORT_SHA"
+
+
+if [[ "$1" == "end" ]]; then
+    TEXT="Pipeline status: $3%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG%0ATag:+$CI_COMMIT_TAG%0ACommit:$CI_COMMIT_SHORT_SHA"
+fi
+
+curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT&silent=$SILENT" $URL > /dev/null
